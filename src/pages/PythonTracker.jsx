5e8c8767f183b7265b2ad59ffef633fa310ac2b8
Shubham Bhogal <163554272+bhoga08@users.noreply.github.com>
2026-01-22 20:55:51 +0530

    Update PythonTracker.jsx

--- src/pages/PythonTracker.jsx
+++ src/pages/PythonTracker.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './JavaTracker.css'; // Reusing same CSS theme
+import './javatracker.css'; // Reusing same CSS theme
 
 const pythonTopics = [
   'Introduction to Python',
